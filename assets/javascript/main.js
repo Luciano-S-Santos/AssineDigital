061d1398a33feeafd41478822706f3170ff47f8d
Luciano Pablo Souza Santos <lucianopablo2002@gmail.com>
2022-10-14 14:07:39 -0300

    bkg navegação

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,20 @@
-var nav = document.getElementById('nav');
+//***********************Heder***********************
+//-nav
+var nav = document.getElementById('navegation');
+var t = document.querySelector('li');
+var teste = t.querySelectorAll('.nav-option')
+
 window.addEventListener("scroll", function(event) {
-            if(window.pageYOffset>100){
+    if(window.pageYOffset>100){
 
-                nav.style.background = "#007bff";
+        nav.style.background = "White";
+        nav.style.transition = ".3s";
+        teste.style.background = "black";
 
-            }
-            else{
-                nav.style.background = "transparent";
-            }
-        });
+    }
+    else{
+        nav.style.background = "transparent";
+    }
+});
+
+//-nav
