80eb37ece4a1b323e342c715cccb99911e83fca4
Luciano Pablo Souza Santos <lucianopablo2002@gmail.com>
2022-10-14 12:11:00 -0300

    organizando estilo

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+var nav = document.getElementById('nav');
+window.addEventListener("scroll", function(event) {
+            if(window.pageYOffset>100){
+
+                nav.style.background = "#007bff";
+
+            }
+            else{
+                nav.style.background = "transparent";
+            }
+        });
