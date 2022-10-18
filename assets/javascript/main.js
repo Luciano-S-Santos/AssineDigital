bb66695774ef651a4c2548f17b11694bf55d8f99
Luciano Pablo Souza Santos <lucianopablo2002@gmail.com>
2022-10-18 20:05:33 -0300

    alteração de style

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,13 @@
 //***********************Heder***********************
 //-nav
 var nav = document.getElementById('navegation');
-var t = document.querySelector('li');
-var teste = t.querySelectorAll('.nav-option')
 
 window.addEventListener("scroll", function(event) {
     if(window.pageYOffset>100){
 
         nav.style.background = "White";
         nav.style.transition = ".3s";
-        teste.style.background = "black";
-
+        nav.style.zIndex = "9";
     }
     else{
         nav.style.background = "transparent";
